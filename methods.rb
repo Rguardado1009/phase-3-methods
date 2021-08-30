6f2ae792ca80d35fb992dc84065b3a1e6dcd6085
Rguardado1009 <rguardado1009@gmail.com>
2021-08-30 12:33:48 -0400

    commit tree 350ea0b807062cf553dd38e630d575a70325a0f2

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
-# Your code here!
+def greet_programmer()
+    
+    puts "Hello, programmer!"
+    
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+    
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+    
+end
+
+def add(num1, num2)
+    num1 + num2
+end
+puts add(2, 5)
+
+def halve(number)
+    if typeof != "number"
+        puts nil
+    end
+    else   puts number/2
+        end
